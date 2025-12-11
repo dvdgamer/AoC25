b214ffe0a79c00db37660b2826cfcdebf177b7c1
david <davidnmponte@gmail.com>
2025-12-11 14:49:54 +0100

    day1 part2 fully working

--- day1/day1-pt2.go
+++ day1/day1-pt2.go
@@ -22,29 +22,44 @@ func main() {
 	for scanner.Scan() {
 		line := scanner.Text()
 		number, err := strconv.Atoi(line[1:])
+
 		if err != nil {
 			log.Fatal(err)
 		}
+
 		if line[0] == 'L' {
-			number = number % 100
+			for number > 99 {
+				number -= 100
+				res++
+			}
+			if dial == number {
+				res++
+			}
 			if number > dial {
+				if dial == 0 {
+					res--
+				}
 				dial = dial + 100
+				res++
 			}
 			dial -= number
 		} else if line[0] == 'R' {
+			if number > 99 {
+				for number > 99 {
+					number -= 100
+					res++
+				}
+			}
 			dial += number
+			if dial > 99 {
+				dial -= 100
+				res++
+			}
 		} else {
 			log.Fatal(err)
 		}
-		if dial > 99 {
-			dial = dial % 100
-		}
-		if dial == 0 {
-			res++
-		}
-		fmt.Println(dial)
 	}
-	fmt.Println("res:", res)
+	fmt.Println("\nres:", res)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
