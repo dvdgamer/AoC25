2c9d1b5b178a01bf78e2aef2d506645ce350da34
david <davidnmponte@gmail.com>
2025-12-12 15:17:03 +0100

    added pseudo code for the checker function

--- day2/day2.go
+++ day2/day2.go
@@ -8,19 +8,23 @@ import (
 	"strings"
 )
 
-func checker(val int, len int) int {
-	return 1
+func checker(val int, stringVal string, len int) int {
+	// slice number in half
+	// check if halved numbers are the same
+	// return number
+	return 0
 }
 
 func main() {
-
 	content, err := os.ReadFile("./test-input.txt")
 	// content, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	text := string(content)
+	var text string
+
+	text = string(content)
 	text = strings.ReplaceAll(text, "\n", "")
 	array := strings.Split(text, ",")
 	fmt.Print(array)
@@ -38,8 +42,9 @@ func main() {
 
 		numberLen := len(strconv.Itoa(firstVal))
 		for firstVal < secondVal {
+			firstStringVal := strconv.Itoa(firstVal)
 			if numberLen%2 == 0 {
-				checker(firstVal, numberLen)
+				checker(firstVal, firstStringVal, numberLen)
 			}
 			firstVal++
 		}
